fb0ac6b07330970f697cc243443cecb116df17a3
mxjwkv <mxjwkv@gmail.com>
2015-09-03 18:45:48 -0700

    Update prototype include file

--- libfunc.h
+++ libfunc.h
@@ -1,6 +1,6 @@
 // libfunc.h
 //     Created: 26-AUG-2015
-// Last Update: 02-SEP-2015
+// Last Update: 03-SEP-2015
 
 /***
 #include <stdio.h>
@@ -17,3 +17,5 @@ using namespace std;
 
 int lib_test_incr_num(int i);
 int lib_test_decr_num(int i);
+char *getLibFuncBuildInfo(char *szBuildInfo, int nBuildInfoLen);
+
