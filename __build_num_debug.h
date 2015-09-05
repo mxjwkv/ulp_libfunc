dbb8e35f7ff960fa64c8dde39b80ff7e3cf8c986
mxjwkv <mxjwkv@gmail.com>
2015-09-05 16:47:56 -0700

    build

--- __build_num_debug.h
+++ __build_num_debug.h
@@ -1,14 +1,14 @@
 // __build_num_debug.h
 
-// Created: 2015-09-03 18:24:29 -0700
-//  by ucx_incrbuildnum (Ver 4 - Built: Sep  3 2015 17:05:43)
+// Created: 2015-09-05 16:47:01 -0700
+//  by ucx_incrbuildnum (Ver 5 - Built: Sep  5 2015 11:02:17)
 
 #ifndef _BUILD_NUM_H_
 #define _BUILD_NUM_H_
 
-#define BUILD_DATTIM "2015-09-03 18:24:29 -0700"
-#define BUILD_MACHINE "Main_Work_Desktop_W7_G25_VM_Ubuntu14"
-#define BUILD_NUM 2
-#define BUILD_NUM_TXT "2"
+#define BUILD_DATTIM "2015-09-05 16:47:01 -0700"
+#define BUILD_MACHINE "Work_Laptop_W7_G20_VM_Ubuntu14"
+#define BUILD_NUM 3
+#define BUILD_NUM_TXT "3"
 
 #endif // _BUILD_NUM_H_
